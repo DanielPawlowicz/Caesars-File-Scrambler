4c8c6ee10d67dfe2d5470d55e5a558a27e1ffa4a
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-04-06 19:23:37 +0200

    Main frame settings

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -4,6 +4,16 @@ public class MainFrame extends JFrame{
 	
 	public MainFrame() {
 		
+		setTitle("Caesar Cipher Scrambler");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(500, 500);
+		setLocationRelativeTo(null);
+		
+		ActionChooser actionChooser = new ActionChooser();
+		
+		this.add(actionChooser);
+		
+		this.setVisible(true);
 	}
 
 }
