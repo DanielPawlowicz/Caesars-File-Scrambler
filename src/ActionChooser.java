12eefadc221eae2183251c1b45f8860dc04133f6
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-04-06 19:46:58 +0200

    Action chooser: enrypt / decrypt buttons

--- src/ActionChooser.java
+++ src/ActionChooser.java
@@ -1,8 +1,32 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class ActionChooser extends JPanel{
 
-	
+	public ActionChooser() {
+		JButton encryptionButton = new JButton("Encrypt file");
+		JButton decryptionButton = new JButton("Decrypt file");
+		
+		encryptionButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+//				System.out.println("works e");
+				new Encrypter();
+			}
+		});
+		
+		decryptionButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+//				System.out.println("works d");
+				new Decrypter();
+			}
+		});
+		
+		this.add(encryptionButton);
+		this.add(decryptionButton);
+	}
 	
 	
 }
